066fb7fa34940045627cc0ed8e23c2d69b8de287
awesomeLucas <adfr486233@gmail.com>
2024-02-21 07:22:12 +0800

    Add DefaultblueprintCard and My Blueprint Card

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,15 @@
-import { BlueprintCard } from './components/BlueprintCard';
-import { NormalBlueprintCard } from './components/BlueprintCard/normal';}
-import DogNFT from './assets/images/Imagedog.svg';
-import Robot from './assets/images/robot.svg';
-
+import robot from './assets/svg/robot.svg';
+import { DefaultBlueprintCard } from './components/Cards/default';
 function App() {
   return (
-    <div className="flex gap-2 items-center">
-      <BlueprintCard
-        imageLink={DogNFT}
-        name={'Color Dog'}
-        blueprintid={5}
-        tsupply={100000000}
-        mintprice={0.000153}
-        mintlimit={100}
-      />
-
-      <NormalBlueprintCard
-        imageLink={Robot}
-        name={'Color '}
-        blueprintid={5}
-        tsupply={100000000}
-        address={'0x55d398326f99059ff775485246999027b3197955'}
+    <div className="flex flex-col gap-2">
+      <DefaultBlueprintCard
+        imageLink={robot}
+        name="Robot"
+        blueprintid={28}
+        tsupply={1000000000}
+        mintPrice={0.001}
+        mintLimit={100}
       />
     </div>
   );
