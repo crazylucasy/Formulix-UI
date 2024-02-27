fee417edfa4e5eced475402676a3689b0686e2c5
awesomeLucas <adfr486233@gmail.com>
2024-02-28 05:05:12 +0800

    feat:add contexts for RainbowKit Provider

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,4 @@
+import Providers from './contexts/RainbowKitProviders';
 import CoreRoutes from './routes';
 import BlueprintInfoCard from './components/Cards/BlueprintInfoCard/BlueprintInfoCard';
 import BlueprintUpdateCard from './components/Cards/BlueprintInfoCard/BlueprintUpdateCard';
@@ -22,95 +23,98 @@ import pickAxe from './assets/images/development/pickaxe_iron_wood_ERC1155.webp'
 import woodShield from './assets/images/development/Shield_wood_ERC1155.webp';
 import ironShield from './assets/images/development/Shield_iron_ERC1155.webp';
 import BlueprintCard from './components/Cards/BlueprintCard/BlueprintCard';
+
 function App() {
   return (
-    <div className="flex flex-col gap-2 justify-center items-center">
-      <BlueprintCard
-        uri={copper}
-        name="copper"
-        blueprintid={5}
-        totalSupply={10000}
-        mintPrice={1000}
-        mintLimit={100}
-        myCardBadge={true}
-        button={true}
-      />
+    <Providers>
+      <div className="flex flex-col gap-2 justify-center items-center">
+        <BlueprintCard
+          uri={copper}
+          name="copper"
+          blueprintid={5}
+          totalSupply={10000}
+          mintPrice={1000}
+          mintLimit={100}
+          myCardBadge={true}
+          button={true}
+        />
 
-      <ApproveBlueprintModal />
-      <DecomposeProductModal />
+        <ApproveBlueprintModal />
+        <DecomposeProductModal />
 
-      <MintProductModal />
+        <MintProductModal />
 
-      <ERC20DecomposeListCard
-        uri={pickAxe}
-        type={'Blueprint'}
-        name={'PickAxe Blueprint'}
-        address={'0xdAC17F958D2ee523a2206206994597C13D831ec7'}
-        amount={100}
-      />
-      <ERC721DecomposeListCard
-        uri={copper}
-        type={'ERC20'}
-        name={'Copper ERC20'}
-        address={'0xdAC17F958D2ee523a2206206994597C13D831ec7'}
-        id={3}
-      />
-      <ERC1155DecomposeListCard
-        uri={key721}
-        type={'ERC721'}
-        name={'Key 721'}
-        address={'0xdAC17F958D2ee523a2206206994597C13D831ec7'}
-        id={3}
-        amount={100}
-      />
+        <ERC20DecomposeListCard
+          uri={pickAxe}
+          type={'Blueprint'}
+          name={'PickAxe Blueprint'}
+          address={'0xdAC17F958D2ee523a2206206994597C13D831ec7'}
+          amount={100}
+        />
+        <ERC721DecomposeListCard
+          uri={copper}
+          type={'ERC20'}
+          name={'Copper ERC20'}
+          address={'0xdAC17F958D2ee523a2206206994597C13D831ec7'}
+          id={3}
+        />
+        <ERC1155DecomposeListCard
+          uri={key721}
+          type={'ERC721'}
+          name={'Key 721'}
+          address={'0xdAC17F958D2ee523a2206206994597C13D831ec7'}
+          id={3}
+          amount={100}
+        />
 
-      <ERC20MintListCard
-        uri={copper}
-        type={'ERC20'}
-        name={'Copper ERC20'}
-        address={'0xdAC17F958D2ee523a2206206994597C13D831ec7'}
-        amount={100}
-      />
-      <ERC721MintListCard
-        uri={key721}
-        type={'ERC721'}
-        name={'Key 721'}
-        address={'0xdAC17F958D2ee523a2206206994597C13D831ec7'}
-        id={3}
-      />
-      <ERC1155MintListCard
-        uri={axeIron}
-        type={'ERC1155'}
-        name={'Axe Iron ERC1155'}
-        address={'0xdAC17F958D2ee523a2206206994597C13D831ec7'}
-        id={3}
-        amount={100}
-      />
+        <ERC20MintListCard
+          uri={copper}
+          type={'ERC20'}
+          name={'Copper ERC20'}
+          address={'0xdAC17F958D2ee523a2206206994597C13D831ec7'}
+          amount={100}
+        />
+        <ERC721MintListCard
+          uri={key721}
+          type={'ERC721'}
+          name={'Key 721'}
+          address={'0xdAC17F958D2ee523a2206206994597C13D831ec7'}
+          id={3}
+        />
+        <ERC1155MintListCard
+          uri={axeIron}
+          type={'ERC1155'}
+          name={'Axe Iron ERC1155'}
+          address={'0xdAC17F958D2ee523a2206206994597C13D831ec7'}
+          id={3}
+          amount={100}
+        />
 
-      <SortBy />
+        <SortBy />
 
-      <BlueprintUpdateCard />
-      <BlueprintInfoCard />
-      <AdvancedFilter />
-      <ProductBlueprintCard
-        imageLink={woodShield}
-        name="WoodShield"
-        blueprintid={28}
-        tsupply={100}
-        address="0x55d398326f99059ff775485246999027b3197955"
-      />
+        <BlueprintUpdateCard />
+        <BlueprintInfoCard />
+        <AdvancedFilter />
+        <ProductBlueprintCard
+          imageLink={woodShield}
+          name="WoodShield"
+          blueprintid={28}
+          tsupply={100}
+          address="0x55d398326f99059ff775485246999027b3197955"
+        />
 
-      <OwnBlueprintCard
-        uri={ironShield}
-        name="WoodShield"
-        blueprintid={28}
-        totalSupply={100}
-        address="0x55d398326f99059ff775485246999027b3197955"
-        myCardBadge={true}
-      />
+        <OwnBlueprintCard
+          uri={ironShield}
+          name="WoodShield"
+          blueprintid={28}
+          totalSupply={100}
+          address="0x55d398326f99059ff775485246999027b3197955"
+          myCardBadge={true}
+        />
 
-      <CoreRoutes />
-    </div>
+        <CoreRoutes />
+      </div>
+    </Providers>
   );
 }
 
